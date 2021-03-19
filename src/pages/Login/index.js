b12ec253e09e0216f0a5bea3e6a02b2cccb4e472
Renato Rosseto <renato_rosseto_neto@hotmail.com>
2021-03-19 20:41:48 -0300

    Adicionando Ant Design ao projeto

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,16 @@
 import React from 'react' 
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css';
 
 import './styles.scss'
 
 export default function Login() {
   return (
-    <h1>Login Page</h1>
+    <button 
+      type="submit" 
+      class="ant-btn ant-btn-secondary ant-btn-lg sc-dmlrTW kSBIxJ"
+    >
+      <span>Entrar</span>
+    </button>
   )
 }
